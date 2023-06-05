d02018de2499e844f8a8aab0b20c5df641ca6059
BastaMasta <bastamasta3150@gmail.com>
2023-06-05 07:43:42 +0530

    Program Complete! added main loop moved money input to Coins.take_inp() method made coffee.

--- coffee_machine/src/main.rs
+++ coffee_machine/src/main.rs
@@ -12,61 +12,37 @@ fn main() {
         coffee: 100,
         money: 0.0,
     };
-    let mut coins = Coins {
-        pennies : 0,
-        nickels : 0,
-        dimes : 0,
-        quarters : 0,
-    };
     loop {
-        let espresso = String::from("espresso");
-        let latte = String::from("latte");
-        let cappuccino = String::from("cappuccino");
+
         println!("What would you like? (espresso/latte/cappuccino):");
         let mut inp_coffee = String::new();
         io::stdin().read_line(&mut inp_coffee).expect("Failed to read input!");
 
         let mut coins = Coins {
-            pennies : 0,
-            nickels : 0,
-            dimes : 0,
-            quarters : 0,
+            pennies : 0.0,
+            nickels : 0.0,
+            dimes : 0.0,
+            quarters : 0.0,
         };
-        let mut temp_str = String::new();
-        println!("Enter number of pennies:");
-        io::stdin().read_line(&mut temp_str).expect("Failed to read input!");
-        coins.pennies = temp_str.trim().parse().expect("please enter valid number!");
-        temp_str.clear();
-        println!("Enter number of nickels:");
-        io::stdin().read_line(&mut temp_str).expect("Failed to read input!");
-        coins.nickels = temp_str.trim().parse().expect("please enter valid number!");
-        temp_str.clear();
-        println!("Enter number of dimes:");
-        io::stdin().read_line(&mut temp_str).expect("Failed to read input!");
-        coins.dimes = temp_str.trim().parse().expect("please enter valid number!");
-        temp_str.clear();
-        println!("Enter number of quarters:");
-        io::stdin().read_line(&mut temp_str).expect("Failed to read input!");
-        coins.quarters = temp_str.trim().parse().expect("please enter valid number!");
-        temp_str.clear();
 
-        println!("{:?}", coins);
-
-
-
-        if espresso.to_lowercase().starts_with(&inp_coffee) {
-
+        if inp_coffee.trim().to_lowercase() == "espresso" {
+            coins.take_inp();
+            coffee_machine.make(CoffeeType::Espresso, coins.calculate());
+        } else if inp_coffee.trim().to_lowercase() == "latte" {
+            coins.take_inp();
+            coffee_machine.make(CoffeeType::Latte, coins.calculate());
+        } else if inp_coffee.trim().to_lowercase() == "cappuccino" {
+            coins.take_inp();
+            coffee_machine.make(CoffeeType::Cappuccino, coins.calculate());
+        } else if inp_coffee.trim().to_lowercase() == "report".to_string() {
+            coffee_machine.report();
         }
-        break;
+        else if (inp_coffee.trim().to_lowercase() == "quit")
+            || (inp_coffee.trim().to_lowercase() == "q")
+            || (inp_coffee.trim().to_lowercase() == "exit") {
+            println!("Thanks for using my coffee machine!");
+            break;
+        }
     }
-
-
-    /*coffee_machine.report();
-
-    let coffee_type = CoffeeType::Espresso;
-    let money = 4.00;
-
-    coffee_machine.make(&coffee_type, money);
-    coffee_machine.report();*/
 
 }
